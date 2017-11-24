c6a95034f73af6181cc97a305878cc49dad1acd0
Thomas Bauer <t001.bauer@t-online.de>
2017-11-24 18:56:21 +0100

    certificates, now 5 and meaningful

--- public_education/initcertificate.sh
+++ public_education/initcertificate.sh
@@ -1,11 +1,11 @@
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.acme.biznet.Certificate",  
-   "certificateId": "1",  
+   "certificateId": "9",  
    "description": "Master of Computer Science",  
    "type": "Master of Science",  
    "issueDate": "2017-11-24T15:55:39.534Z",  
-   "issuer": "org.acme.biznet.Issuer#2",
-   "owner": "org.acme.biznet.Owner#3"
+   "issuer": "org.acme.biznet.Issuer#1",
+   "owner": "org.acme.biznet.Owner#5"
  }' 'http://localhost:3000/api/Certificate'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
@@ -15,7 +15,7 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
    "type": "Master of Science",  
    "issueDate": "2017-09-24T15:55:39.534Z",  
    "issuer": "org.acme.biznet.Issuer#2",
-   "owner": "org.acme.biznet.Owner#4"
+   "owner": "org.acme.biznet.Owner#1"
  }' 'http://localhost:3000/api/Certificate'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
@@ -24,6 +24,26 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
    "description": "Master of Computer Science",  
    "type": "Master of Science",  
    "issueDate": "2017-10-24T15:55:39.534Z",  
+   "issuer": "org.acme.biznet.Issuer#7",
+   "owner": "org.acme.biznet.Owner#12"
+ }' 'http://localhost:3000/api/Certificate'
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "$class": "org.acme.biznet.Certificate",  
+   "certificateId": "4",  
+   "description": "Handwerksmeister Elektro",  
+   "type": "Meisterbrief",  
+   "issueDate": "2017-06-11T15:55:39.534Z",  
    "issuer": "org.acme.biznet.Issuer#5",
-   "owner": "org.acme.biznet.Owner#8"
+   "owner": "org.acme.biznet.Owner#11"
+ }' 'http://localhost:3000/api/Certificate'
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "$class": "org.acme.biznet.Certificate",  
+   "certificateId": "5",  
+   "description": "Handwerksmeister Sanitär- und Heizung",  
+   "type": "Gesellenbrief",  
+   "issueDate": "2002-06-22T15:55:39.534Z",  
+   "issuer": "org.acme.biznet.Issuer#5",
+   "owner": "org.acme.biznet.Owner#2"
  }' 'http://localhost:3000/api/Certificate'
