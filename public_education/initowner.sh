9c7382fe27da95bac1ea27a223b0ebcce793c97f
Thomas Bauer <t001.bauer@t-online.de>
2017-11-24 20:23:08 +0100

    ohne Namespace

--- public_education/initowner.sh
+++ public_education/initowner.sh
@@ -3,83 +3,83 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
    "ownerId": "1",  
    "ownerName": "Goerge Orwell",  
    "ownerNationality": "United Kingdom"  
- }' 'http://localhost:3000/api/org.acme.biznet.Owner'
+ }' 'http://localhost:3000/api/Owner'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.acme.biznet.Owner",  
    "ownerId": "2",  
    "ownerName": "Hugo Egon Balder",  
    "ownerNationality": "Deutschland"  
- }' 'http://localhost:3000/api/org.acme.biznet.Owner'
+ }' 'http://localhost:3000/api/Owner'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.acme.biznet.Owner",  
    "ownerId": "3",  
    "ownerName": "Brad Pitt",  
    "ownerNationality": "United States of America"  
- }' 'http://localhost:3000/api/org.acme.biznet.Owner'
+ }' 'http://localhost:3000/api/Owner'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.acme.biznet.Owner",  
    "ownerId": "4",  
    "ownerName": "Bud Spencer",  
    "ownerNationality": "Italia"  
- }' 'http://localhost:3000/api/org.acme.biznet.Owner'
+ }' 'http://localhost:3000/api/Owner'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.acme.biznet.Owner",  
    "ownerId": "5",  
    "ownerName": "Senta Berger",  
    "ownerNationality": "Austria"  
- }' 'http://localhost:3000/api/org.acme.biznet.Owner'
+ }' 'http://localhost:3000/api/Owner'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.acme.biznet.Owner",  
    "ownerId": "6",  
    "ownerName": "Ryan Gosling",  
    "ownerNationality": "Canadian"  
- }' 'http://localhost:3000/api/org.acme.biznet.Owner'
+ }' 'http://localhost:3000/api/Owner'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.acme.biznet.Owner",  
    "ownerId": "7",  
    "ownerName": "Sophia Loren",  
    "ownerNationality": "Italia"  
- }' 'http://localhost:3000/api/org.acme.biznet.Owner'
+ }' 'http://localhost:3000/api/Owner'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.acme.biznet.Owner",  
    "ownerId": "8",  
    "ownerName": "Colin Farrell",  
    "ownerNationality": "Ireland"  
- }' 'http://localhost:3000/api/org.acme.biznet.Owner'
+ }' 'http://localhost:3000/api/Owner'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.acme.biznet.Owner",  
    "ownerId": "9",  
    "ownerName": "Hildgard Knef",  
    "ownerNationality": "Deutschland"  
- }' 'http://localhost:3000/api/org.acme.biznet.Owner'
+ }' 'http://localhost:3000/api/Owner'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.acme.biznet.Owner",  
    "ownerId": "10",  
    "ownerName": "Ursula Andress",  
    "ownerNationality": "Schweiz"  
- }' 'http://localhost:3000/api/org.acme.biznet.Owner'
+ }' 'http://localhost:3000/api/Owner'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.acme.biznet.Owner",  
    "ownerId": "11",  
    "ownerName": "Marlene Dietrich",  
    "ownerNationality": "Deutsch"  
- }' 'http://localhost:3000/api/org.acme.biznet.Owner'
+ }' 'http://localhost:3000/api/Owner'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.acme.biznet.Owner",  
    "ownerId": "12",  
    "ownerName": "Woody Allen",  
    "ownerNationality": "United States of America"  
- }' 'http://localhost:3000/api/org.acme.biznet.Owner'
+ }' 'http://localhost:3000/api/Owner'
 
 
