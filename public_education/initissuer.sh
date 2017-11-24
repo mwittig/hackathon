a3394bedff3258ef26f89beb678e9f46a1a1949f
Thomas Bauer <t001.bauer@t-online.de>
2017-11-24 17:46:21 +0100

    Testdaten for initial load

---
+++ public_education/initissuer.sh
@@ -0,0 +1,48 @@
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "$class": "org.acme.biznet.Issuer",  
+   "issuerId": "1",  
+   "issuerName": "Hochschule für angewandte Wissenschaften", 
+   "issuerCountry": "Deutschland" 
+ }' 'http://localhost:3000/api/org.acme.biznet.Issuer'
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "$class": "org.acme.biznet.Issuer",  
+   "issuerId": "2",  
+   "issuerName": "University of London", 
+   "issuerCountry": "United Kingdom" 
+ }' 'http://localhost:3000/api/org.acme.biznet.Issuer'
+
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "$class": "org.acme.biznet.Issuer",  
+   "issuerId": "3",  
+   "issuerName": "Universidad Complutense de Madrid", 
+   "issuerCountry": "Espana" 
+ }' 'http://localhost:3000/api/org.acme.biznet.Issuer'
+
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "$class": "org.acme.biznet.Issuer",  
+   "issuerId": "4",  
+   "issuerName": "Universidade de Lisboa", 
+   "issuerCountry": "Portugal" 
+ }' 'http://localhost:3000/api/org.acme.biznet.Issuer'
+
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "$class": "org.acme.biznet.Issuer",  
+   "issuerId": "5",  
+   "issuerName": "IHK München und Oberbayern", 
+   "issuerCountry": "Deutschland" 
+ }' 'http://localhost:3000/api/org.acme.biznet.Issuer'
+
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "$class": "org.acme.biznet.Issuer",  
+   "issuerId": "6",  
+   "issuerName": "Technische Universität München", 
+   "issuerCountry": "Deutschland" 
+ }' 'http://localhost:3000/api/org.acme.biznet.Issuer'
+
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "$class": "org.acme.biznet.Issuer",  
+   "issuerId": "7",  
+   "issuerName": "Ludwig-Maximiliams-Universität München", 
+   "issuerCountry": "Deutschland" 
+ }' 'http://localhost:3000/api/org.acme.biznet.Issuer'
