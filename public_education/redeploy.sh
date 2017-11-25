8941c67c54f1b5dc7a07f1f200b87315033b825c
Marcus Wittig <WittigMarcus@gmail.com>
2017-11-25 14:57:16 +0100

    added command lines arg for composer-rest-server

--- public_education/redeploy.sh
+++ public_education/redeploy.sh
@@ -1,3 +1,3 @@
 composer archive create -t dir -n . -a public_education@0.0.1.bna
 composer network update -a public_education@0.0.1.bna -c admin@public_education
-composer-rest-server
+composer-rest-server --card admin@public_education --namespaces never
